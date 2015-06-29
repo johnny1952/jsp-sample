ffe93466170bf6dc6abbd88c8edaa152a2095319
johnny <lee>
2015-06-29 18:10:52 +0800

    first commit

---
+++ src/com/atopcloud/sample/ViewImage.java
@@ -0,0 +1,61 @@
+package com.atopcloud.sample;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ViewImage
+ */
+@WebServlet("/ViewImage")
+public class ViewImage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewImage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("image/jpeg");
+		OutputStream os = response.getOutputStream();
+		byte[] buffer=new byte[8192];
+		String imageName=request.getParameter("name");   
+		FileInputStream fis=new FileInputStream(imageName);
+		int count=0;
+		
+		//读取并传输指定的图像数据
+		while(true)
+		{
+			count=fis.read(buffer);
+			if(count == -1)
+				break;
+			os.write(buffer, 0, count);
+		}
+		fis.close();
+	}
+}
